afc3db4f0c08e5840b7e7f7a92a940735d9b0fea
Rafael Menezes <rafael.menezes@aluno.usj.edu.br>
2020-06-22 19:20:26 -0300

    Criada DebitosLiquidados

---
+++ ArquivoRedeCard/src/br/com/redecard/classes/DebitosLiquidados.java
@@ -0,0 +1,190 @@
+package br.com.redecard.classes;
+
+public class DebitosLiquidados {
+
+	private String tipoRegistro;
+	private String numeroPV;
+	private String numeroOrdemDebito;
+	private String dataOrdemDebito;
+	private String valorTotalDebito;
+	private String identificadorMotivoAjuste;
+	private String descricaoMotivoAjuste;
+	private String numeroCartao;
+	private String numeroUSU;
+	private String dataCVNSU;
+	private String numAutorizacao;
+	private String valorTransacaoOriginal;
+	private String numRVOriginal;
+	private String dataRVOriginal;
+	private String numPVOriginal;
+	private String nCartaReferencia;
+	private String dataCancelamento;
+	private String numeroProcesso;
+	private String mesReferencia;
+	private String valorPago;
+	private String dataLiquidacao;
+	private String processoRet;
+	private String identficadorCompensado;
+	private String descricaoCompensado;
+	public String getTipoRegistro() {
+		return tipoRegistro;
+	}
+	public void setTipoRegistro(String tipoRegistro) {
+		this.tipoRegistro = tipoRegistro;
+	}
+	public String getNumeroPV() {
+		return numeroPV;
+	}
+	public void setNumeroPV(String numeroPV) {
+		this.numeroPV = numeroPV;
+	}
+	public String getNumeroOrdemDebito() {
+		return numeroOrdemDebito;
+	}
+	public void setNumeroOrdemDebito(String numeroOrdemDebito) {
+		this.numeroOrdemDebito = numeroOrdemDebito;
+	}
+	public String getDataOrdemDebito() {
+		return dataOrdemDebito;
+	}
+	public void setDataOrdemDebito(String dataOrdemDebito) {
+		this.dataOrdemDebito = dataOrdemDebito;
+	}
+	public String getValorTotalDebito() {
+		return valorTotalDebito;
+	}
+	public void setValorTotalDebito(String valorTotalDebito) {
+		this.valorTotalDebito = valorTotalDebito;
+	}
+	public String getIdentificadorMotivoAjuste() {
+		return identificadorMotivoAjuste;
+	}
+	public void setIdentificadorMotivoAjuste(String identificadorMotivoAjuste) {
+		this.identificadorMotivoAjuste = identificadorMotivoAjuste;
+	}
+	public String getDescricaoMotivoAjuste() {
+		return descricaoMotivoAjuste;
+	}
+	public void setDescricaoMotivoAjuste(String descricaoMotivoAjuste) {
+		this.descricaoMotivoAjuste = descricaoMotivoAjuste;
+	}
+	public String getNumeroCartao() {
+		return numeroCartao;
+	}
+	public void setNumeroCartao(String numeroCartao) {
+		this.numeroCartao = numeroCartao;
+	}
+	public String getNumeroUSU() {
+		return numeroUSU;
+	}
+	public void setNumeroUSU(String numeroUSU) {
+		this.numeroUSU = numeroUSU;
+	}
+	public String getDataCVNSU() {
+		return dataCVNSU;
+	}
+	public void setDataCVNSU(String dataCVNSU) {
+		this.dataCVNSU = dataCVNSU;
+	}
+	public String getNumAutorizacao() {
+		return numAutorizacao;
+	}
+	public void setNumAutorizacao(String numAutorizacao) {
+		this.numAutorizacao = numAutorizacao;
+	}
+	public String getValorTransacaoOriginal() {
+		return valorTransacaoOriginal;
+	}
+	public void setValorTransacaoOriginal(String valorTransacaoOriginal) {
+		this.valorTransacaoOriginal = valorTransacaoOriginal;
+	}
+	public String getNumRVOriginal() {
+		return numRVOriginal;
+	}
+	public void setNumRVOriginal(String numRVOriginal) {
+		this.numRVOriginal = numRVOriginal;
+	}
+	public String getDataRVOriginal() {
+		return dataRVOriginal;
+	}
+	public void setDataRVOriginal(String dataRVOriginal) {
+		this.dataRVOriginal = dataRVOriginal;
+	}
+	public String getNumPVOriginal() {
+		return numPVOriginal;
+	}
+	public void setNumPVOriginal(String numPVOriginal) {
+		this.numPVOriginal = numPVOriginal;
+	}
+	public String getnCartaReferencia() {
+		return nCartaReferencia;
+	}
+	public void setnCartaReferencia(String nCartaReferencia) {
+		this.nCartaReferencia = nCartaReferencia;
+	}
+	public String getDataCancelamento() {
+		return dataCancelamento;
+	}
+	public void setDataCancelamento(String dataCancelamento) {
+		this.dataCancelamento = dataCancelamento;
+	}
+	public String getNumeroProcesso() {
+		return numeroProcesso;
+	}
+	public void setNumeroProcesso(String numeroProcesso) {
+		this.numeroProcesso = numeroProcesso;
+	}
+	public String getMesReferencia() {
+		return mesReferencia;
+	}
+	public void setMesReferencia(String mesReferencia) {
+		this.mesReferencia = mesReferencia;
+	}
+	public String getValorPago() {
+		return valorPago;
+	}
+	public void setValorPago(String valorPago) {
+		this.valorPago = valorPago;
+	}
+	public String getDataLiquidacao() {
+		return dataLiquidacao;
+	}
+	public void setDataLiquidacao(String dataLiquidacao) {
+		this.dataLiquidacao = dataLiquidacao;
+	}
+	public String getProcessoRet() {
+		return processoRet;
+	}
+	public void setProcessoRet(String processoRet) {
+		this.processoRet = processoRet;
+	}
+	public String getIdentficadorCompensado() {
+		return identficadorCompensado;
+	}
+	public void setIdentficadorCompensado(String identficadorCompensado) {
+		this.identficadorCompensado = identficadorCompensado;
+	}
+	public String getDescricaoCompensado() {
+		return descricaoCompensado;
+	}
+	public void setDescricaoCompensado(String descricaoCompensado) {
+		this.descricaoCompensado = descricaoCompensado;
+	}
+	@Override
+	public String toString() {
+		return "DebitosLiquidados [tipoRegistro=" + tipoRegistro + ", numeroPV=" + numeroPV + ", numeroOrdemDebito="
+				+ numeroOrdemDebito + ", dataOrdemDebito=" + dataOrdemDebito + ", valorTotalDebito=" + valorTotalDebito
+				+ ", identificadorMotivoAjuste=" + identificadorMotivoAjuste + ", descricaoMotivoAjuste="
+				+ descricaoMotivoAjuste + ", numeroCartao=" + numeroCartao + ", numeroUSU=" + numeroUSU + ", dataCVNSU="
+				+ dataCVNSU + ", numAutorizacao=" + numAutorizacao + ", valorTransacaoOriginal="
+				+ valorTransacaoOriginal + ", numRVOriginal=" + numRVOriginal + ", dataRVOriginal=" + dataRVOriginal
+				+ ", numPVOriginal=" + numPVOriginal + ", nCartaReferencia=" + nCartaReferencia + ", dataCancelamento="
+				+ dataCancelamento + ", numeroProcesso=" + numeroProcesso + ", mesReferencia=" + mesReferencia
+				+ ", valorPago=" + valorPago + ", dataLiquidacao=" + dataLiquidacao + ", processoRet=" + processoRet
+				+ ", identficadorCompensado=" + identficadorCompensado + ", descricaoCompensado=" + descricaoCompensado
+				+ "]";
+	}
+	
+	
+	
+}
