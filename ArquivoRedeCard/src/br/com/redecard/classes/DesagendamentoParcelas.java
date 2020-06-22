ca0eca2a48e4f1f346254ceb0086af9955123f52
Rafael Menezes <rafael.menezes@aluno.usj.edu.br>
2020-06-22 19:46:08 -0300

    Criada classe DesagendamentoParcelas

---
+++ ArquivoRedeCard/src/br/com/redecard/classes/DesagendamentoParcelas.java
@@ -0,0 +1,145 @@
+package br.com.redecard.classes;
+
+public class DesagendamentoParcelas {
+	
+	private String tipoRegistro;
+	private String originalPV;
+	private String originalRV;
+	private String numDocumento;
+	private String dataEmissao;
+	private String dataCredito;
+	private String novoValorParcela;
+	private String valorOriginalParcela;
+	private String valorAjuste;
+	private String dataCancelamento;
+	private String valorRVOriginal;
+	private String valorCancelamentoSolicitado;
+	private String numeroCartao;
+	private String dataTransacao;
+	private String Nsu;
+	private String tipoDebito;
+	private String numeroParcela;
+	private String bandeiraRVOrigem;
+	public String getTipoRegistro() {
+		return tipoRegistro;
+	}
+	public void setTipoRegistro(String tipoRegistro) {
+		this.tipoRegistro = tipoRegistro;
+	}
+	public String getOriginalPV() {
+		return originalPV;
+	}
+	public void setOriginalPV(String originalPV) {
+		this.originalPV = originalPV;
+	}
+	public String getOriginalRV() {
+		return originalRV;
+	}
+	public void setOriginalRV(String originalRV) {
+		this.originalRV = originalRV;
+	}
+	public String getNumDocumento() {
+		return numDocumento;
+	}
+	public void setNumDocumento(String numDocumento) {
+		this.numDocumento = numDocumento;
+	}
+	public String getDataEmissao() {
+		return dataEmissao;
+	}
+	public void setDataEmissao(String dataEmissao) {
+		this.dataEmissao = dataEmissao;
+	}
+	public String getDataCredito() {
+		return dataCredito;
+	}
+	public void setDataCredito(String dataCredito) {
+		this.dataCredito = dataCredito;
+	}
+	public String getNovoValorParcela() {
+		return novoValorParcela;
+	}
+	public void setNovoValorParcela(String novoValorParcela) {
+		this.novoValorParcela = novoValorParcela;
+	}
+	public String getValorOriginalParcela() {
+		return valorOriginalParcela;
+	}
+	public void setValorOriginalParcela(String valorOriginalParcela) {
+		this.valorOriginalParcela = valorOriginalParcela;
+	}
+	public String getValorAjuste() {
+		return valorAjuste;
+	}
+	public void setValorAjuste(String valorAjuste) {
+		this.valorAjuste = valorAjuste;
+	}
+	public String getDataCancelamento() {
+		return dataCancelamento;
+	}
+	public void setDataCancelamento(String dataCancelamento) {
+		this.dataCancelamento = dataCancelamento;
+	}
+	public String getValorRVOriginal() {
+		return valorRVOriginal;
+	}
+	public void setValorRVOriginal(String valorRVOriginal) {
+		this.valorRVOriginal = valorRVOriginal;
+	}
+	public String getValorCancelamentoSolicitado() {
+		return valorCancelamentoSolicitado;
+	}
+	public void setValorCancelamentoSolicitado(String valorCancelamentoSolicitado) {
+		this.valorCancelamentoSolicitado = valorCancelamentoSolicitado;
+	}
+	public String getNumeroCartao() {
+		return numeroCartao;
+	}
+	public void setNumeroCartao(String numeroCartao) {
+		this.numeroCartao = numeroCartao;
+	}
+	public String getDataTransacao() {
+		return dataTransacao;
+	}
+	public void setDataTransacao(String dataTransacao) {
+		this.dataTransacao = dataTransacao;
+	}
+	public String getNsu() {
+		return Nsu;
+	}
+	public void setNsu(String nsu) {
+		Nsu = nsu;
+	}
+	public String getTipoDebito() {
+		return tipoDebito;
+	}
+	public void setTipoDebito(String tipoDebito) {
+		this.tipoDebito = tipoDebito;
+	}
+	public String getNumeroParcela() {
+		return numeroParcela;
+	}
+	public void setNumeroParcela(String numeroParcela) {
+		this.numeroParcela = numeroParcela;
+	}
+	public String getBandeiraRVOrigem() {
+		return bandeiraRVOrigem;
+	}
+	public void setBandeiraRVOrigem(String bandeiraRVOrigem) {
+		this.bandeiraRVOrigem = bandeiraRVOrigem;
+	}
+	@Override
+	public String toString() {
+		return "DesagendamentoParcelas [tipoRegistro=" + tipoRegistro + ", originalPV=" + originalPV + ", originalRV="
+				+ originalRV + ", numDocumento=" + numDocumento + ", dataEmissao=" + dataEmissao + ", dataCredito="
+				+ dataCredito + ", novoValorParcela=" + novoValorParcela + ", valorOriginalParcela="
+				+ valorOriginalParcela + ", valorAjuste=" + valorAjuste + ", dataCancelamento=" + dataCancelamento
+				+ ", valorRVOriginal=" + valorRVOriginal + ", valorCancelamentoSolicitado="
+				+ valorCancelamentoSolicitado + ", numeroCartao=" + numeroCartao + ", dataTransacao=" + dataTransacao
+				+ ", Nsu=" + Nsu + ", tipoDebito=" + tipoDebito + ", numeroParcela=" + numeroParcela
+				+ ", bandeiraRVOrigem=" + bandeiraRVOrigem + "]";
+	}
+	
+	
+	
+}
